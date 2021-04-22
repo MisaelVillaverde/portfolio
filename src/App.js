0e9e073eeb98455ab1d302658d21f1fac051479b
misaelvillaverde <misaelvilla10@gmail.com>
2021-04-22 00:53:53 -0500

    responsive titlebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Card from './components/Card';
 import TitleBar from './components/TitleBar';
 
 function App() {
   return (
     <>
       <TitleBar />
+      {/* <Card title='Amazon Clone.' /> */}
     </>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ title, sourceImage, info }) => {
+  return (
+    <div className='card__container'>
+      <div className='card'>
+        <h2 className='card__title'>{title}</h2>
+        <img src={sourceImage} alt='' className='card__image' />
+        <p className='card__info'>{info}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
