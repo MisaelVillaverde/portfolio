f12ccc9c55a350b3bc3b1fd035be87a715d48a65
misaelvillaverde <misaelvilla10@gmail.com>
2021-05-08 00:16:20 -0500

    nav buttons, hero bg, skills component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import './Hero.scss';
+import React from "react";
+import "./Hero.scss";
+import Skills from "./Skills";
 
 const Hero = () => {
   const handleScroll = () => {
     window.scroll({
       top: 0, // cuando tenga lista las cartas vuelvo
       left: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
-    <div className='hero__container'>
-      <a
-        rel="noreferrer"
-        className='title__container'
-        href='https://www.linkedin.com/in/misael-villaverde/'
-        target='_blank'
-      >
-        <h1>
-          Hi, I'm <br /> <b>Misael Villaverde</b>
-        </h1>
-        <h2>Web Developer & Graphic Designer</h2>
-      </a>
-      <div className='scroll' onClick={handleScroll}>
-        <span>Scroll to see my projects</span>
-        <i class='fas fa-chevron-down'></i>
+    <div className="hero__container">
+      <div className="ts__container">
+        <div className="title__container">
+          <h1>
+            Hi, I'm <br /> <b>Misael Villaverde</b>
+          </h1>
+          <h2>Web Developer & Graphic Designer</h2>
+        </div>
+        <div className="social__container">
+          <a
+            rel="noreferrer"
+            href="https://www.github.com/misaelvillaverde"
+            target="_blank"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+          <a
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/misael-villaverde/"
+            target="_blank"
+          >
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a rel="noreferrer" href="https://wa.me/50765552598" target="_blank">
+            <i class="fab fa-whatsapp"></i>
+          </a>
+        </div>
+      </div>
+      <Skills />
+      <div className="scroll" onClick={handleScroll}>
+        <span>Portfolio</span>
+        <i class="fas fa-chevron-down"></i>
       </div>
     </div>
   );
---
+++ src/components/Skills.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Skills.scss";
+
+const Skills = () => {
+  return (
+    <ul className="skills__container">
+      <li>
+        <i class="fab fa-js"></i>
+      </li>
+      <li>
+        <i class="fab fa-sass"></i>
+      </li>
+      <li>
+        <i class="fab fa-react"></i>
+      </li>
+    </ul>
+  );
+};
+
+export default Skills;
