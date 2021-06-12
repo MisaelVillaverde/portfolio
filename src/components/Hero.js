e5900894789aec1f8954339002599300e3568886
misaelvillaverde <misaelvilla10@gmail.com>
2021-06-12 00:43:54 -0500

    [UPDATE] show more relevant skills

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,26 @@
-import React from "react";
-import "./Hero.scss";
-import Skills from "./Skills";
-import { motion } from "framer-motion";
+import React from 'react';
+import './Hero.scss';
+import Skills from './Skills';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   const handleScroll = () => {
     window.scroll({
       top: window.innerHeight,
       left: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
   return (
-    <div className="hero__container">
-      <div className="ts__container">
+    <div className='hero__container'>
+      <div className='ts__container'>
         <motion.div
-          className="title__container"
+          className='title__container'
           whileHover={{
             y: -5,
             transition: {
-              type: "spring",
+              type: 'spring',
               damping: 0,
               duration: 2,
             },
@@ -29,37 +29,37 @@ const Hero = () => {
           <h1>
             Hi, I'm <br /> <b>Misael Villaverde</b>
           </h1>
-          <h2>Web Developer & Graphic Designer</h2>
+          <h2>FullStack Web Developer</h2>
         </motion.div>
-        <div className="social__container">
+        <div className='social__container'>
           <a
-            rel="noreferrer"
-            href="https://www.github.com/misaelvillaverde"
-            target="_blank"
-            className="gh"
+            rel='noreferrer'
+            href='https://www.github.com/misaelvillaverde'
+            target='_blank'
+            className='gh'
           >
-            <i className="fab fa-github"></i>
+            <i className='fab fa-github'></i>
           </a>
           <a
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/misael-villaverde/"
-            target="_blank"
-            className="linkedin"
+            rel='noreferrer'
+            href='https://www.linkedin.com/in/misael-villaverde/'
+            target='_blank'
+            className='linkedin'
           >
-            <i className="fab fa-linkedin-in"></i>
+            <i className='fab fa-linkedin-in'></i>
           </a>
           <a
-            rel="noreferrer"
-            href="https://wa.me/50765552598"
-            target="_blank"
-            className="wa"
+            rel='noreferrer'
+            href='https://wa.me/50765552598'
+            target='_blank'
+            className='wa'
           >
-            <i className="fab fa-whatsapp"></i>
+            <i className='fab fa-whatsapp'></i>
           </a>
         </div>
       </div>
       <Skills />
-      <div className="scroll" onClick={handleScroll}>
+      <div className='scroll' onClick={handleScroll}>
         <span>Portfolio</span>
         <motion.i
           animate={{
@@ -68,10 +68,10 @@ const Hero = () => {
           transition={{
             repeat: Infinity,
             duration: 0.5,
-            ease: "easeInOut",
-            repeatType: "reverse",
+            ease: 'easeInOut',
+            repeatType: 'reverse',
           }}
-          className="fas fa-chevron-down"
+          className='fas fa-chevron-down'
         ></motion.i>
       </div>
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,42 +1,25 @@
-import React from "react";
-import { ReactComponent as Figma } from "../assets/svg/figma.svg";
+import React from 'react';
 
-import "./Skills.scss";
+import './Skills.scss';
 
 const Skills = () => {
   return (
-    <ul className="skills__container">
-      <li 
-        whileHover={{
-          scale: 1.1,
-          rotateZ: -4
-        }}>
-        <i className="fab fa-js"></i>
-      </li>
-      <li
-        whileHover={{
-          rotateY: 360,
-          transition: {
-            duration: 0.5
-          }
-        }}
-      >
-        <i className="fab fa-sass"></i>
-      </li>
-      <li
-        whileHover={{
-          rotate: 180,
-          transition: {
-            repeat: Infinity,
-            ease: "linear",
-            duration: 1,
-          },
-        }}
-      >
-        <i className="fab fa-react"></i>
+    <ul className='skills__container'>
+      <li>
+        <i className='fab fa-js'></i>
       </li>
       <li>
-        <Figma as="i"/>
+        <img
+          src='https://camo.githubusercontent.com/349b918c3c7a45f8271ae7812246d95bd1dc20b220e8543067ce78c3cdb79c57/68747470733a2f2f342e62702e626c6f6773706f742e636f6d2f2d583755506b4f516a5175512f5775484c55454d375344492f4141414141414141414f592f72584758534f66505032636b465f63534f433343356433425f426849674e637841434c63424741732f73313630302f6d6f6e676f646225324225323532383225323532392e706e67'
+          alt='mongoDB'
+          width='140px'
+        />
+      </li>
+      <li>
+        <i className='fab fa-react'></i>
+      </li>
+      <li>
+        <i class='fab fa-node'></i>
       </li>
     </ul>
   );
